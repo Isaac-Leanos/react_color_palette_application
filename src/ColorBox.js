46e8d2234d3fe6d267ed2ea25f84305477bb7355
Isaac-Leanos <isaac.leanos11@gmail.com>
2019-04-23 20:20:29 -0700

    copy-to-clipboard npm package implementation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./ColorBox.css";
-
+import {CopyToClipboard} from "react-copy-to-clipboard"; // copy color hexcode to clipboard
 
 
 const ColorBox = ({name, background})=>{
     return (
-        <div style={{background: background}} className="color-box">
-            <div className="copy-container">
-                <div className="box-content">
-                    <span>{name}</span>
+        <CopyToClipboard text={background}>
+            <div style={{background: background}} className="color-box">
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
                 </div>
-                <button className="copy-button">Copy</button>
+                <span className="see-more">More</span>
             </div>
-            <span className="see-more">More</span>
-        </div>
+        </CopyToClipboard>
     )
 }
-
 
 
 
--- src/Palette.js
+++ src/Palette.js
@@ -1,13 +1,10 @@
-import React, {useEffect} from "react";
+import React from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
 
 
 const Pallete = ({colors, emoji, id, paletteName})=>{
 
-    // useEffect(()=>{
-    //     p
-    // },[])
     const colorBoxes = colors.map(color =>{
         return <ColorBox background={color.color} name={color.name} />
     })
