d186f0a06e43470875791bbfb9af69fd12226220
Isaac-Leanos <isaac.leanos11@gmail.com>
2019-04-24 09:08:10 -0700

    added copy effect/cue

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ColorBox.css";
 import {CopyToClipboard} from "react-copy-to-clipboard"; // copy color hexcode to clipboard
 
 
 const ColorBox = ({name, background})=>{
+    const [copied, setCopied] = useState(false);
+    const changeCopyState = ()=>{
+        setCopied(true);
+        setTimeout(()=>{
+            setCopied(false);
+        },1300)
+    }
     return (
-        <CopyToClipboard text={background}>
+        <CopyToClipboard text={background} onCopy={changeCopyState}>
             <div style={{background: background}} className="color-box">
-                <div className="copy-container">
-                    <div className="box-content">
-                        <span>{name}</span>
+                <div style={{background: background}} className={`copy-overlay ${copied ? "show" : ""}`} /> {/*overlay*/}
+                    <div className="copy-container"> 
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">Copy</button>
                     </div>
-                    <button className="copy-button">Copy</button>
-                </div>
-                <span className="see-more">More</span>
+                    <span className="see-more">More</span>
             </div>
         </CopyToClipboard>
     )
