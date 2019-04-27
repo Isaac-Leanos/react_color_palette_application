bc5cbffc91431bdcc12ce2d9a0841a0369f0716e
Isaac-Leanos <isaac.leanos11@gmail.com>
2019-04-26 21:21:47 -0700

    Select component which allows for user to switch from hex to rgb or rgba

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {Select, MenuItem} from '@material-ui/core';
 import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
 import "./Navbar.css";
@@ -6,7 +7,8 @@ import "./Navbar.css";
 
 const Navbar = (props)=>{
    
-    const {level, changeLevel} = props;
+    const [format, setFormat] = useState("HEX")
+    const {level, changeLevel, changeFormatUp} = props;
 
     const marks = {
         100: '',
@@ -20,6 +22,11 @@ const Navbar = (props)=>{
         900: '',
       };
 
+      const changeFormat = (e)=>{
+          setFormat(e.target.value);
+          changeFormatUp(e.target.value)
+      }
+
     return (
         <header className="navbar">
             <div className="logo">
@@ -30,6 +37,13 @@ const Navbar = (props)=>{
                 <div className="slider">
                     <Slider defaultValue={level} min={100} max={900} onAfterChange={changeLevel} step={100} marks={marks} />
                 </div>                
+            </div>
+            <div className="select">
+                <Select value={format} onChange={changeFormat}>
+                    <MenuItem value="hex">HEX - #777777</MenuItem>
+                    <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+                    <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1)</MenuItem>
+                </Select>
             </div>
         </header>
     )
--- src/Palette.js
+++ src/Palette.js
@@ -7,20 +7,26 @@ import Navbar from "./Navbar";
 const Pallete = (props)=>{
     const {colors, id, emoji, paletteName} = props.palette;
 
+    //states
     const [level, setLevel] = useState(500);
+    const [format, setFormat] = useState("hex");
     console.log(props.palette);
 
     const colorBoxes = colors[level].map(color =>{
-        return <ColorBox background={color.hex} name={color.name} />
+        return <ColorBox background={color[format]} name={color.name} />
     })
 
     const changeLevel = (L)=>{
         setLevel(L);
     }
     
+    const changeFormat =(val)=>{
+        setFormat(val);
+    }
+
     return (
         <div className="palette" >
-            <Navbar level={level} changeLevel={changeLevel}></Navbar>
+            <Navbar level={level} changeLevel={changeLevel} changeFormatUp={changeFormat}/>
             <div className="palette-colors">{colorBoxes}</div>
         </div>
     )
