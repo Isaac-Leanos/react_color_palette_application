66280aa20a86a7e1859eaa690fc2d6a5e0eba672
Isaac-Leanos <isaac.leanos11@gmail.com>
2019-04-29 14:05:24 -0700

    homescreen display of mini palettes utilizing css grids

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,17 +1,47 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 
 import { withStyles} from "@material-ui/styles";
 
 const styles = {
-    main: {
-        background: "pink"
+    root: {
+        background: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        "&:hover": {
+            cursor: "pointer"
+        },
+        // overflow: "hidden"
+    },
+    colors: {
+        background: "grey"
+    },
+    title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative",
+    },
+    emoji: {
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
     }
 }
 
 
-const MiniPalette = ({classes})=>{
+const MiniPalette = ({classes, id, paletteName, emoji, colors})=>{
     return (
-        <div className={classes.main}>ok</div>
+        <div className={classes.root}>
+            <div className={classes.colors}></div>
+            <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+        </div>
     )
 }
 
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,24 +1,58 @@
-import React, {  } from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
 import MiniPalette from "./MiniPalette";
 import { withStyles} from "@material-ui/styles";
 
 
+const styles = {
+    root: {
+        background: "blue",
+        height: "100vh",
+        display: "flex",
+        // alignItems: "center", 
+        alignItems: "flex-start", 
+        justifyContent: "center"
+    },
+    container: {
+        border: "1px solid red",
+        width: "50%",
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
+    nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    miniPalettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "30% 30% 30%",
+        gridGap: "5%"
+    }
+}
 
 
 // homescreen 
-const PaletteList = ({palettes})=>{
+const PaletteList = ({palettes, classes})=>{
       return (
-          <div>
-          <MiniPalette/>
-            {palettes.map(pal=>(
-                    <Link to={`/palette/${pal.id}`}>
-                        <h1>{pal.paletteName}</h1>
-                    </Link>
-                // </MiniPalette>
-            ))}
-        </div>
+          <div className={classes.root}>
+
+
+            <div className={classes.container}>
+                <nav className={classes.nav}><h1>React Color Palettes</h1></nav>
+                <div className={classes.miniPalettes}>
+                    {palettes.map(pal=>(
+                        <MiniPalette {...pal}></MiniPalette>
+                    ))}
+                </div>
+            </div>
+
+
+          </div>
     )
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
